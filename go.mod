d9369f6de8d718ac15afa323311cf84ecf2b2e9b
scointin <samson.cointin@gmail.com>
2024-09-11 14:01:53 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module netto-catto
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+const LogoAscii = "\x1b[32m" + `
+         _nnnn_
+        dGGGGMMb
+       @p~qp~~qMb
+       M|@||@) M|
+       @,----.JM|
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    '.       | '' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMP|   .'
+     '-'       '-
+` + "\x1b[0m"
+
+func main() {
+
+}
