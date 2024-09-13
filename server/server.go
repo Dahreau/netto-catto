5cb225c753373ed233e2468c89889af52787358d
student <darosamaky@gmail.com>
2024-09-13 10:58:59 +0200

    added server.go to daro branch

--- server/server.go
+++ server/server.go
@@ -1,1 +1,53 @@
-hihi
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run . <port>")
+		os.Exit(1)
+	}
+
+	port := os.Args[1]
+
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Starting server on port %s...\n", port)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Printf("Error accepting connection: %v\n", err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println(conn)
+	buf := make([]byte, 1024)
+
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Printf("Error reading from connection: %v\n", err)
+			break
+		}
+
+		fmt.Println("You typed : ", string(buf[:n]))
+	}
+
+}
